a8dac3a876189beab61f14d7e0536fd35b4ef929
DOZEN <kyosuke6428@gmail.com>
2012-01-05 03:18:42 +0900

    new file:   favicon.ico new file:   n-psptter.png new file:   send.php new file:   twitteroauth/OAuth.php new file:   twitteroauth/twitteroauth.php

---
+++ send.php
@@ -0,0 +1,43 @@
+<?php
+
+require 'class.php';
+try {
+  $twitter = new Twitter();
+} catch (Exception $e) {
+  //ログインしてなかったときの処理
+  Authering::Redirect();
+}
+//投稿系の処理。
+if (isset($_POST['tweet'])) {
+  if ($_COOKIE['footer'] && !$_POST['id']) {
+    $tweet = $_POST['tweet'] . ' ' . $_COOKIE['footer'];
+  } else {
+    $tweet = $_POST['tweet'];
+  }
+  $tweet = aa($tweet);
+  $twitter->Tweet('tweet', array('tweet' => $tweet, 'id' => $_POST['id']));
+} else if (isset($_GET['retweet'])) {
+  $twitter->Tweet('retweet', array('id' => $_GET['retweet']));
+} else if (isset($_GET['fav'])) {
+  $twitter->Tweet('fav', array('id' => $_GET['fav']));
+} else if (isset($_GET['fav_dest'])) {
+  $twitter->Tweet('fav_dest', array('id' => $_GET['fav_dest']));
+} else if (isset($_GET['destroy'])) {
+  $twitter->Tweet('destroy', array('id' => $_GET['destroy']));
+} else if ($_GET['tm'] == 'follow') {
+  $twitter->Tweet('follow', array('user_id' => $_GET['user_id']));
+} else if ($_GET['tm'] == 'remove') {
+  $twitter->Tweet('remove', array('user_id' => $_GET['user_id']));
+}
+$status = $twitter->Status();
+if ($status->http_code != 200) {
+  if ($statis->http_code == 403) {
+    $url = './?message=同じ内容のツイートはできません＞＜；';
+  } else {
+    $url = './?message=エラーが発生しました。ツイート出来なかったかも知れないです＞＜； error code:' . $status->http_code;
+  }
+} else {
+  $url = './';
+}
+header("Location: $url");
+?>
