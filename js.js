7acbc0daef02cdff03b03a4c33baf01769066d07
DOZEN <kyosuke6428@gmail.com>
2012-01-05 03:38:07 +0900

    送信パラメータの仕様変更

--- js.js
+++ js.js
@@ -21,7 +21,7 @@ String.prototype.replaceAll = function (org, dest){
 }
 
 var request = null;
-function makeRequest(user_id, count, tm) {
+function makeRequest(id, count, sendtype) {
   if(typeof window.XMLHttpRequest != 'undefined') {
     try {
       request = new XMLHttpRequest();
@@ -37,11 +37,7 @@ function makeRequest(user_id, count, tm) {
       document.getElementById(count).innerHTML=('\u614c\u3066\u306a\u3044\u3067\uff01');
     }
   }
-  if (tm == 'follow' || tm == 'remove') {
-    url = 'http://npsptter.dip.jp/sendlojax.php?tm='+tm+'&user_id='+user_id;
-    params = null;
-  }
-  request.open('GET', url, true);
+  request.open(POST, 'http://npsptter.dip.jp/sendlojax.php', true);
   request.setRequestHeader('Content-type', 'text/plain;charset=utf-8');
-  request.send(params);
+  request.send(type + '=' + id);
 }
--- sendlojax.php
+++ sendlojax.php
@@ -8,18 +8,18 @@ try {
   Authering::Redirect();
 }
 //投稿系の処理。
-if (isset($_GET['retweet'])) {
-  $twitter->Tweet('retweet', array('id' => $_GET['retweet']));
-} else if (isset($_GET['fav'])) {
-  $twitter->Tweet('fav', array('id' => $_GET['fav']));
-} else if (isset($_GET['fav_dest'])) {
-  $twitter->Tweet('fav_dest', array('id' => $_GET['fav_dest']));
-} else if (isset($_GET['destroy'])) {
-  $twitter->Tweet('destroy', array('id' => $_GET['destroy']));
-} else if ($_GET['tm'] == 'follow') {
-  $twitter->Tweet('follow', array('user_id' => $_GET['user_id']));
-} else if ($_GET['tm'] == 'remove') {
-  $twitter->Tweet('remove', array('user_id' => $_GET['user_id']));
+if (isset($_POST['retweet'])) {
+  $twitter->Tweet('retweet', array('id' => $_POST['retweet']));
+} else if (isset($_POST['fav'])) {
+  $twitter->Tweet('fav', array('id' => $_POST['fav']));
+} else if (isset($_POST['fav_dest'])) {
+  $twitter->Tweet('fav_dest', array('id' => $_POST['fav_dest']));
+} else if (isset($_POST['destroy'])) {
+  $twitter->Tweet('destroy', array('id' => $_POST['destroy']));
+} else if (isset($_POST['follow'])) {
+  $twitter->Tweet('follow', array('user_id' => $_POST['user_id']));
+} else if (isset($_POST['remove'])) {
+  $twitter->Tweet('remove', array('user_id' => $_POST['user_id']));
 }
 $status = $twitter->Status();
 if ($status->http_code != 200) {
